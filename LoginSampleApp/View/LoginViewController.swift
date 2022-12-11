6cd0f5e2aea1e1f38896dfb28b58e947e64f48cd
hemg2 <hemg2@naver.com>
2022-12-11 23:06:54 +0900

    stackView

---
+++ LoginSampleApp/View/LoginViewController.swift
@@ -0,0 +1,20 @@
+//
+//  LoginViewController.swift
+//  LoginSampleApp
+//
+//  Created by 1 on 2022/12/11.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    
+    
+    
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        navigationController?.navigationBar.isHidden = true
+    }
+}
